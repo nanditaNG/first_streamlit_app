962703ae24ecec3d87cff01d1d84633c06508022
Nandita <gurwaranan@gmail.com>
2022-06-21 00:43:08 -0400

    updating the app

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #just writes the data to the screen
-streamlit.text(fruityvice_response.json())
+#streamlit.text(fruityvice_response.json())
 
 #Take the json version of the response and normalise it
 
