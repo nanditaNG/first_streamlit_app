9ac01f6353b03c242d070f0141b405bf5a265d06
Nandita <gurwaranan@gmail.com>
2022-06-22 01:05:18 -0400

    adding more functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,17 +43,19 @@ except URLError as e:
     streamlit.error()
 
 #connecting to snowflake via the credentials entered in streamlit
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 
-#Creating a cursor
-my_cur = my_cnx.cursor()
-#Using the cursor to execute sql
-
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+streamlit.header("The fruit load list contains: ")
+#Snowflake-related functions
+def get_fruit_load_list():
+    with my_cnx.cursor as my_cur:
+        my_cur.execute("select * from fruit_load_list")
+        return my_cur.fetchall()
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 #my_data_row = my_cur.fetchone()
-my_data_rows = my_cur.fetchall()
-streamlit.header("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+    my_data_rows = get_fruit_load_list()
+    streamlit.dataframe(my_data_rows)
 
 #New Section to display fruityvice api response
 
