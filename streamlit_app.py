3f89d9141a142f51b43a002b66364ea8d8b970b6
Nandita <gurwaranan@gmail.com>
2022-06-22 01:19:29 -0400

    adding more functions

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,9 +62,16 @@ def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
         my_cur.execute("insert into fruit_load_list values ('" +new_fruit+"')")
         return "Thanks for adding " +new_fruit
+
 add_my_fruit= streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a fruit to the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     streamlit.text(back_from_function)
+
+if streamlit.button('Get fruit list'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    my_data_rows = get_fruit_load_list()
+    my_cnx.close()
+    streamlit.dataframe(my_data_rows)
 
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
